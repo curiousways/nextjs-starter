40ba62d73ffe653a33e2349d4ae15c2c3acc186d
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-06-28 11:55:17 +0100

    Update next config file

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
     GOOGLE_ANALYTICS_ID: "yourgaid",
     FATHOM_ANALYTICS_ID: "yourfathomanalyticsid",
   },
+  images: {
+    deviceSizes: [400, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
 };
 
 module.exports = nextConfig;
